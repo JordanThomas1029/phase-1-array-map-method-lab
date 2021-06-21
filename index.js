8971e8934e929bb84baa5ac77f2b3bf4ae133144
Jordan Thomas <tjordan706@gmail.com>
2021-06-21 17:27:50 -0400

    commit tree ff4955ad325cad0e6adb7f9b5b89cac2aba95949

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  let text = tutorials.map((swap) => swap.split(" "));
+  let array = [];
+  let newtext = text.map(function (tutor) {
+    console.log(tutor);
+    tutor.map(function (element) {
+      array.push(element[0].charAt(0).toUpperCase() + element[0].slice(1));
+    });
+    //return tutor[0].charAt(0).toUpperCase() + tutor[0].slice(1);
+  });
+  return array;
 }
